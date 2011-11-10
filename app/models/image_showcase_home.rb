c1d658c1e3ffa456281b7f439f5faabfe25b1d9a
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2011-11-09 23:43:31 -0600

    Fix showcase home images to actually work on put and post

--- app/models/image_showcase_home.rb
+++ app/models/image_showcase_home.rb
@@ -3,8 +3,8 @@ class ImageShowcaseHome < ActiveRecord::Base
   belongs_to :image
   belongs_to :showcase_home
 
-  before_save do |image_page|
-    image_page.position = (ImagePage.maximum(:position) || -1) + 1
+  before_save do |image_showcase_home|
+    image_showcase_home.position = (ImageShowcaseHome.maximum(:position) || -1) + 1
   end
 
 end
--- app/models/showcase_home.rb
+++ app/models/showcase_home.rb
@@ -4,9 +4,28 @@ class ShowcaseHome < ActiveRecord::Base
   
   has_many :image_showcase_homes
   has_many :images, :through => :image_showcase_homes, :order => 'position ASC'
-  accepts_nested_attributes_for :images, :allow_destroy => false
-  
 
   validates :address, :presence => true, :uniqueness => true
-  
+  accepts_nested_attributes_for :images, :allow_destroy => false
+  attr_accessible :address, :custom_teaser, :description, :specifics, :price, :sale_date, :images_attributes
+
+  def images_attributes=(data)
+    ImageShowcaseHome.delete_all(:showcase_home_id => self.id)
+
+    (0..(data.length-1)).each do |i|
+      unless (image_data = data[i.to_s]).nil? or image_data['id'].blank?
+        image_showcase_home = self.image_showcase_homes.new(:image_id => image_data['id'].to_i, :position => i)
+        # Add caption if supported
+        if RefinerySetting.find_or_set(:showcase_home_images_captions, false)
+          image_showcase_home.caption = image_data['caption']
+        end
+        self.image_showcase_homes << image_showcase_home
+      end
+    end
+  end
+
+  def caption_for_image_index(index)
+    self.image_showcase_homes[index].try(:caption).presence || ""
+  end
+
 end
